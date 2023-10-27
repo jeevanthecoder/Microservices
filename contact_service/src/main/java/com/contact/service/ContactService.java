7eaf72b0e939acf55d6908a4ea63e0d467f7e7d2
jeevanthecoder <98173940+jeevanthecoder@users.noreply.github.com>
2023-10-27 18:05:49 +0530

    Delete contact_service directory

--- contact_service/src/main/java/com/contact/service/ContactService.java
+++
@@ -1,8 +0,0 @@
-package com.contact.service;
-import com.contact.entity.Contacts;
-
-import java.util.List;
-
-public interface ContactService {
-    public List<Contacts> getContactsOfUser(Long userId);
-}
